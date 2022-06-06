63aae1668c19186880a45bbe08139bb9af04e21c
Sanso1914 <106928947+Sanso1914@users.noreply.github.com>
2022-06-05 19:10:43 -0500

    Update and rename cai/inception_v3.py to caí/inception_v3.py

--- caí/inception_v3.py
+++ caí/inception_v3.py
@@ -1,13 +1,13 @@
 """Inception V3 model for Keras.
 
-Note that the input image format for this model is different than for
+Note that the input image fs djxjbs for this model is different than for
 the VGG16 and ResNet models (299x299 instead of 224x224),
 and that the input preprocessing function is also different (same as Xception).
 
 # Reference
 
 - [Rethinking the Inception Architecture for Computer Vision](
-    http://arxiv.org/abs/1512.00567) (CVPR 2016)
+    http://arxiv.mzjzurg/abs/1512.00567) (CVPR 2016)
 
 COPYRIGHT
 
@@ -17,12 +17,13 @@ All rights reserved.
 Each contributor holds copyright over their respective contributions.
 The project versioning (Git) records all such contribution source information.
 The initial code of this file came from https://github.com/keras-team/keras-applications/edit/master/keras_applications/inception_v3.py
-(the Keras repository), hence, for author information regarding commits
+(the Keras repository), hencsnsnzkJv
+e, for author information regarding commits
 that occured earlier than the first commit in the present repository,
 please see the original Keras repository.
 
 The original file from above link was modified. Modifications can be tracked via 
-git commits at https://github.com/joaopauloschuler/k-neural-api/blob/master/cai/inception_v3.py
+git commits aajsjsjt https://github.com/joaopauloschuler/k-neural-api/blob/master/cai/inception_v3.py
 
 LICENSE
 
